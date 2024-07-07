0bbb05f751cb9260ead5bfbf53ddc4495c564b5f
KENT <kwilliam866@gmail.com>
2024-07-07 10:50:15 +0300

    New Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// divide
+
+function divide(n1,n2){
+    return n1/n2
+
+}
+let equals =divide(2000,100)
+console.log(equals)
+
+// arrow square
+const square=no1 => no1*no1
+square(5)
+
+// arrow add
+let add = (add1,add2) => add1 + add2
+add(25,75)
