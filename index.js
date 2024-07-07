e3b888e3cd03daf6e2510522a0434f93642db26c
KENT <kwilliam866@gmail.com>
2024-07-07 11:03:21 +0300

    new commit

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ console.log(equals)
 
 // arrow square
 const square=no1 => no1*no1
-square(5)
+console.log(square(5))
 
 // arrow add
 let add = (add1,add2) => add1 + add2
-add(25,75)
+console.log(add(25,75))
